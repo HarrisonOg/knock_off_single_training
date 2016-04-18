e21ac451bc1500fc10430a141ee2bef742d56814
Harrison Oglesby <hoglesby@csumb.edu>
2016-04-17 20:18:49 -0700

    got controls working

---
+++ knock_off_single_training/Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	Vector3 offset = Vector3.zero;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ knock_off_single_training/Assets/Scripts/Fist.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fist : MonoBehaviour {
+
+	public float punchPower = 20;
+	private Rigidbody rb;
+	Vector3 offset = new Vector3(0f, 0f, 0f);
+	Vector3 playerPos = new Vector3(0f,0f,0f);
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		playerPos = transform.parent.transform.position;
+		offset = transform.position - playerPos;
+	}
+
+	public void Punch (Vector3 input, bool toDo) {
+		if (toDo) {
+			rb.AddForce (input * punchPower);
+		} else {
+			transform.position = playerPos + offset;
+		}
+	}
+}
